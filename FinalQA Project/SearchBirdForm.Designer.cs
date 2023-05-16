402e38241e7cc2c0a9a2ec2f711a73f240d7f02f
FurmanVlad <vladfurman98@gmail.com>
2023-05-16 23:29:43 +0300

    Added search bird option

---
+++ FinalQA Project/SearchBirdForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace FinalQA_Project
+{
+    partial class SearchBirdForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchBirdSerialTextBox = new System.Windows.Forms.TextBox();
+            this.serialNumberSearchBirdLabel = new System.Windows.Forms.Label();
+            this.GenderComboBox = new System.Windows.Forms.ComboBox();
+            this.SpeciesSearchBirdComboBox = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerSearchBird = new System.Windows.Forms.DateTimePicker();
+            this.genderSearchBirdLabel = new System.Windows.Forms.Label();
+            this.speciesSearchBirdLabel = new System.Windows.Forms.Label();
+            this.hatchDateSearchBirdLabel = new System.Windows.Forms.Label();
+            this.searchBirdButton = new System.Windows.Forms.Button();
+            this.searchOptionComboBox = new System.Windows.Forms.ComboBox();
+            this.chooseSerachOption = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchBirdSerialTextBox
+            // 
+            this.SearchBirdSerialTextBox.Location = new System.Drawing.Point(469, 123);
+            this.SearchBirdSerialTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SearchBirdSerialTextBox.Name = "SearchBirdSerialTextBox";
+            this.SearchBirdSerialTextBox.Size = new System.Drawing.Size(204, 22);
+            this.SearchBirdSerialTextBox.TabIndex = 1;
+            this.SearchBirdSerialTextBox.Visible = false;
+            // 
+            // serialNumberSearchBirdLabel
+            // 
+            this.serialNumberSearchBirdLabel.AutoSize = true;
+            this.serialNumberSearchBirdLabel.Location = new System.Drawing.Point(309, 126);
+            this.serialNumberSearchBirdLabel.Name = "serialNumberSearchBirdLabel";
+            this.serialNumberSearchBirdLabel.Size = new System.Drawing.Size(127, 16);
+            this.serialNumberSearchBirdLabel.TabIndex = 2;
+            this.serialNumberSearchBirdLabel.Text = "Enter Serial Number";
+            this.serialNumberSearchBirdLabel.Visible = false;
+            // 
+            // GenderComboBox
+            // 
+            this.GenderComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.GenderComboBox.FormattingEnabled = true;
+            this.GenderComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderComboBox.Location = new System.Drawing.Point(419, 121);
+            this.GenderComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GenderComboBox.Name = "GenderComboBox";
+            this.GenderComboBox.Size = new System.Drawing.Size(121, 24);
+            this.GenderComboBox.TabIndex = 3;
+            this.GenderComboBox.Visible = false;
+            // 
+            // SpeciesSearchBirdComboBox
+            // 
+            this.SpeciesSearchBirdComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SpeciesSearchBirdComboBox.FormattingEnabled = true;
+            this.SpeciesSearchBirdComboBox.Items.AddRange(new object[] {
+            "American Gouldian",
+            "European Gouldian",
+            "Australian  Gouldian"});
+            this.SpeciesSearchBirdComboBox.Location = new System.Drawing.Point(405, 121);
+            this.SpeciesSearchBirdComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SpeciesSearchBirdComboBox.Name = "SpeciesSearchBirdComboBox";
+            this.SpeciesSearchBirdComboBox.Size = new System.Drawing.Size(183, 24);
+            this.SpeciesSearchBirdComboBox.TabIndex = 4;
+            this.SpeciesSearchBirdComboBox.Visible = false;
+            // 
+            // dateTimePickerSearchBird
+            // 
+            this.dateTimePickerSearchBird.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerSearchBird.Location = new System.Drawing.Point(405, 123);
+            this.dateTimePickerSearchBird.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePickerSearchBird.Name = "dateTimePickerSearchBird";
+            this.dateTimePickerSearchBird.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerSearchBird.TabIndex = 5;
+            this.dateTimePickerSearchBird.Value = new System.DateTime(2023, 5, 16, 0, 0, 0, 0);
+            this.dateTimePickerSearchBird.Visible = false;
+            // 
+            // genderSearchBirdLabel
+            // 
+            this.genderSearchBirdLabel.AutoSize = true;
+            this.genderSearchBirdLabel.Location = new System.Drawing.Point(429, 103);
+            this.genderSearchBirdLabel.Name = "genderSearchBirdLabel";
+            this.genderSearchBirdLabel.Size = new System.Drawing.Size(52, 16);
+            this.genderSearchBirdLabel.TabIndex = 6;
+            this.genderSearchBirdLabel.Text = "Gender";
+            this.genderSearchBirdLabel.Visible = false;
+            // 
+            // speciesSearchBirdLabel
+            // 
+            this.speciesSearchBirdLabel.AutoSize = true;
+            this.speciesSearchBirdLabel.Location = new System.Drawing.Point(429, 102);
+            this.speciesSearchBirdLabel.Name = "speciesSearchBirdLabel";
+            this.speciesSearchBirdLabel.Size = new System.Drawing.Size(57, 16);
+            this.speciesSearchBirdLabel.TabIndex = 7;
+            this.speciesSearchBirdLabel.Text = "Species";
+            this.speciesSearchBirdLabel.Visible = false;
+            // 
+            // hatchDateSearchBirdLabel
+            // 
+            this.hatchDateSearchBirdLabel.AutoSize = true;
+            this.hatchDateSearchBirdLabel.Location = new System.Drawing.Point(429, 103);
+            this.hatchDateSearchBirdLabel.Name = "hatchDateSearchBirdLabel";
+            this.hatchDateSearchBirdLabel.Size = new System.Drawing.Size(74, 16);
+            this.hatchDateSearchBirdLabel.TabIndex = 8;
+            this.hatchDateSearchBirdLabel.Text = "Hatch Date";
+            this.hatchDateSearchBirdLabel.Visible = false;
+            // 
+            // searchBirdButton
+            // 
+            this.searchBirdButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.searchBirdButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.searchBirdButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.searchBirdButton.Location = new System.Drawing.Point(323, 313);
+            this.searchBirdButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.searchBirdButton.Name = "searchBirdButton";
+            this.searchBirdButton.Size = new System.Drawing.Size(128, 46);
+            this.searchBirdButton.TabIndex = 9;
+            this.searchBirdButton.Text = "Search";
+            this.searchBirdButton.UseVisualStyleBackColor = false;
+            this.searchBirdButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // searchOptionComboBox
+            // 
+            this.searchOptionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.searchOptionComboBox.FormattingEnabled = true;
+            this.searchOptionComboBox.Items.AddRange(new object[] {
+            "Serial Number",
+            "Gender",
+            "Hatch Date",
+            "Species"});
+            this.searchOptionComboBox.Location = new System.Drawing.Point(24, 121);
+            this.searchOptionComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.searchOptionComboBox.Name = "searchOptionComboBox";
+            this.searchOptionComboBox.Size = new System.Drawing.Size(199, 24);
+            this.searchOptionComboBox.TabIndex = 10;
+            this.searchOptionComboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // chooseSerachOption
+            // 
+            this.chooseSerachOption.AutoSize = true;
+            this.chooseSerachOption.Location = new System.Drawing.Point(21, 91);
+            this.chooseSerachOption.Name = "chooseSerachOption";
+            this.chooseSerachOption.Size = new System.Drawing.Size(71, 16);
+            this.chooseSerachOption.TabIndex = 11;
+            this.chooseSerachOption.Text = "Search by:";
+            // 
+            // SearchBirdForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chooseSerachOption);
+            this.Controls.Add(this.searchOptionComboBox);
+            this.Controls.Add(this.searchBirdButton);
+            this.Controls.Add(this.hatchDateSearchBirdLabel);
+            this.Controls.Add(this.speciesSearchBirdLabel);
+            this.Controls.Add(this.genderSearchBirdLabel);
+            this.Controls.Add(this.dateTimePickerSearchBird);
+            this.Controls.Add(this.SpeciesSearchBirdComboBox);
+            this.Controls.Add(this.GenderComboBox);
+            this.Controls.Add(this.serialNumberSearchBirdLabel);
+            this.Controls.Add(this.SearchBirdSerialTextBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SearchBirdForm";
+            this.Sizable = false;
+            this.Text = "\'";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox SearchBirdSerialTextBox;
+        private System.Windows.Forms.Label serialNumberSearchBirdLabel;
+        private System.Windows.Forms.ComboBox GenderComboBox;
+        private System.Windows.Forms.ComboBox SpeciesSearchBirdComboBox;
+        private System.Windows.Forms.DateTimePicker dateTimePickerSearchBird;
+        private System.Windows.Forms.Label genderSearchBirdLabel;
+        private System.Windows.Forms.Label speciesSearchBirdLabel;
+        private System.Windows.Forms.Label hatchDateSearchBirdLabel;
+        private System.Windows.Forms.Button searchBirdButton;
+        private System.Windows.Forms.ComboBox searchOptionComboBox;
+        private System.Windows.Forms.Label chooseSerachOption;
+    }
+}
---
+++ FinalQA Project/SearchBirdForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace FinalQA_Project
+{
+    public partial class SearchBirdForm : MaterialForm
+    {
+        private DataGridView dataGridView1 = new DataGridView();
+        public SearchBirdForm()
+        {
+            InitializeComponent();
+            serialNumberSearchBirdLabel.Visible = false;
+            SearchBirdSerialTextBox.Visible = false;
+            genderSearchBirdLabel.Visible = false;
+            GenderComboBox.Visible = false;
+            speciesSearchBirdLabel.Visible = false;
+            SpeciesSearchBirdComboBox.Visible = false;
+            hatchDateSearchBirdLabel.Visible = false;
+            dateTimePickerSearchBird.Visible = false;
+
+        }
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedOption = searchOptionComboBox.SelectedItem.ToString();
+            serialNumberSearchBirdLabel.Visible = false;
+            SearchBirdSerialTextBox.Visible = false;
+            genderSearchBirdLabel.Visible = false;
+            GenderComboBox.Visible = false;
+            speciesSearchBirdLabel.Visible = false;
+            SpeciesSearchBirdComboBox.Visible = false;
+            hatchDateSearchBirdLabel.Visible = false;
+            dateTimePickerSearchBird.Visible = false;
+           
+            if (selectedOption == "Serial Number")
+            {
+                serialNumberSearchBirdLabel.Visible = true;
+                SearchBirdSerialTextBox.Visible = true;
+               
+
+            }
+            else if (selectedOption == "Gender")
+            {
+                // Show the gender combo box
+                genderSearchBirdLabel.Visible = true;
+                GenderComboBox.Visible = true;
+               
+
+            }
+            else if (selectedOption == "Species")
+            {
+               
+                speciesSearchBirdLabel.Visible = true;
+                SpeciesSearchBirdComboBox.Visible = true;
+                
+            }
+            else if (selectedOption == "Hatch Date")
+            {
+
+              
+                hatchDateSearchBirdLabel.Visible = true;
+                dateTimePickerSearchBird.Visible = true;
+
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a new Excel Application object
+            Application excelApp = new Application();
+
+            // Open the Excel workbook containing the login information
+            Workbook workbook = excelApp.Workbooks.Open(@"C:\Users\vladi\source\repos\FinalQA Project\FinalQA Project\Birds habitat.xlsx");
+
+            // Get the Worksheet object for the sheet containing the login information
+            Worksheet worksheet = (Worksheet)workbook.Worksheets["Birds"];
+            string selectedOption = searchOptionComboBox.Text;
+            if (string.IsNullOrEmpty(selectedOption))
+            {
+                MessageBox.Show("Please choose search option.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ClosingAll(excelApp, workbook, worksheet);
+                return;
+            }
+
+            if (selectedOption == "Serial Number")
+            {
+                string serialnum = SearchBirdSerialTextBox.Text;
+
+                if (string.IsNullOrEmpty(serialnum))
+                {
+                    MessageBox.Show("Please enter serial number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ClosingAll(excelApp, workbook, worksheet);
+                    return;
+                }
+                Range serialNumberRange = worksheet.Range["A:A"].Find(serialnum, Missing.Value, XlFindLookIn.xlValues, XlLookAt.xlWhole, XlSearchOrder.xlByRows, XlSearchDirection.xlNext, false, Missing.Value, Missing.Value);
+                if (serialNumberRange == null)
+                {
+                    MessageBox.Show("Bird does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ClosingAll(excelApp, workbook, worksheet);
+                    return;
+                }
+                else
+                {
+                    int columnCount = worksheet.UsedRange.Columns.Count;
+                    object[] rowData = new object[columnCount];
+                    for (int i = 1; i <= columnCount; i++)
+                    {
+                        Range cell = worksheet.Cells[serialNumberRange.Row, i];
+                        rowData[i - 1] = cell.Value;
+                    }
+
+                    if (rowData != null)
+                    {
+                        ShowResultBirdSearch resultForm = new ShowResultBirdSearch();
+                        resultForm.AddRowToDataGridView(rowData);
+                        resultForm.Show();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Row data is null.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        ClosingAll(excelApp, workbook, worksheet);
+                        return;
+                    }
+
+                }
+
+
+            }
+            if (selectedOption == "Gender")
+            {
+                string selectedGender = GenderComboBox.Text;
+                if (string.IsNullOrEmpty(selectedGender))
+                {
+                    MessageBox.Show("Please choose gender.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ClosingAll(excelApp, workbook, worksheet);
+                    return;
+                }
+                SearchBirdFromSheet(worksheet, selectedGender, "E:E");
+
+            }
+            if (selectedOption == "Hatch Date")
+            {
+
+                string selectedDate = dateTimePickerSearchBird.Text;
+                SearchBirdFromSheet(worksheet, selectedDate, "D:D");
+
+
+            }
+            if (selectedOption == "Species")
+            {
+
+                string selectedSpecies = SpeciesSearchBirdComboBox.Text;
+                if (string.IsNullOrEmpty(selectedSpecies))
+                {
+                    MessageBox.Show("Please choose species.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ClosingAll(excelApp, workbook, worksheet);
+                    return;
+                }
+
+                SearchBirdFromSheet(worksheet, selectedSpecies, "B:B");
+            }
+
+            ClosingAll(excelApp, workbook, worksheet);
+            
+
+
+        }
+
+        private void ClosingAll(Application excelApp, Workbook workbook, Worksheet worksheet )
+        {
+            workbook.Close();
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+
+        }
+
+        private void SearchBirdFromSheet(Worksheet worksheet, string comboBoxPick,string col) {
+            ShowResultBirdSearch resultForm = new ShowResultBirdSearch();
+
+            Range range = worksheet.Range[col]; // Assuming the gender column is column E
+
+            Range foundRange = range.Find(comboBoxPick, Missing.Value, XlFindLookIn.xlValues, XlLookAt.xlWhole, XlSearchOrder.xlByRows, XlSearchDirection.xlNext, false, Missing.Value, Missing.Value);
+
+            if (foundRange != null)
+            {
+                Range firstCell = foundRange;
+                List<object[]> matchingRows = new List<object[]>();
+
+                do
+                {
+                    int row = firstCell.Row;
+                    int columnCount = worksheet.UsedRange.Columns.Count;
+                    object[] rowData = new object[columnCount];
+
+                    for (int i = 1; i <= columnCount; i++)
+                    {
+                        Range dataCell = worksheet.Cells[row, i];
+                        rowData[i - 1] = dataCell.Value;
+                    }
+
+                    matchingRows.Add(rowData);
+
+                    firstCell = range.FindNext(firstCell);
+                }
+                while (firstCell != null && firstCell.Address != foundRange.Address);
+
+                foreach (object[] rowData in matchingRows)
+                {
+                    resultForm.AddRowToDataGridView(rowData);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No birds found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            resultForm.Show();
+        }
+           
+    }
+
+ }
+
